bfd70b735ce673f1ecbb9b3575e0f3c5e5d562fa
thichlammoctools-collab <thichlammoctools@gmail.com>
2026-08-24 20:54:48 +0700

    Create fixhash.sql

---
+++ fixnhanh/scripts/fixhash.sql
@@ -0,0 +1,1 @@
+UPDATE passwords SET hash='pbkdf2$25000$YWFhYWFhYWFhYWFhYWFhYQ==$ysSJwSODommM+2JdvkHmQIb9caG2hcpFTrAh9WmPabw=' WHERE user_id IN ('usr_001','usr_002','usr_003','usr_004','usr_005','usr_006');
